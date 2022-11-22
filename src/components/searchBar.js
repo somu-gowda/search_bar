6f2b36343a6dbbe9768668b43698836b38baaffa
somu-gowda <85481833+somu-gowda@users.noreply.github.com>
2022-11-22 19:57:48 +0530

    search bar component

---
+++ src/components/searchBar.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+var data = require("../searchData.json");
+
+const SearchBar = () => {
+  // State
+  const [value, setValue] = useState("");
+
+  // Onchange
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  //   Onsearch
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+  };
+
+  return (
+    <div className="App">
+      <h1>Search</h1>
+
+      <div className="search-container">
+        <div className="search-inner">
+          <input type="text" value={value} onChange={onChange} />
+          <button onClick={() => onSearch(value)}> Search </button>
+        </div>
+        <div className="dropdown">
+          {data &&
+            data.fields
+              .filter((item) => {
+                const searchTerm = value.toLowerCase();
+                const fullName = item.label.toLowerCase();
+
+                return (
+                  searchTerm &&
+                  fullName.startsWith(searchTerm) &&
+                  fullName !== searchTerm
+                );
+              })
+              .slice(0, 10)
+              .map((item) => (
+                <div
+                  onClick={() => onSearch(item.label)}
+                  className="dropdown-row"
+                  key={item.key}
+                >
+                  {item.label}
+                </div>
+              ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SearchBar;
